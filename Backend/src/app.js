1af284139da1f82bdfc90ce2f610cc58b1f86d47
Moon Awan <malikmoon.developer061@gmail.com>
2026-04-08 16:28:41 +0500

    Upgrade dev-push-notification v and cors error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,25 +18,19 @@ connectToDB();
 app.use(cors());
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "https://dev-push-notification.vercel.app",
+    credentials: true,
   }),
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); 
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
+    credentials: true,
+  }),
+);
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 app.use(morgan("dev"));
 
 app.use(express.json({ limit: "10mb" }));
