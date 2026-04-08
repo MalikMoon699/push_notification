f11f1d74abfe82743d7e62c0b11196efb7ba45eb
Moon Awan <malikmoon.developer061@gmail.com>
2026-04-08 16:49:56 +0500

    Update App.js Final

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,3 @@
-import { PORT } from "./config/env.js";
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -13,23 +11,19 @@ import notificationRoutes from "./routes/notification.routes.js";
 import connectToDB from "./database/mongodb.js";
 
 const app = express();
+
 connectToDB();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-api-key",
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-api-key"],
+};
 
-  next();
-});
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(morgan("dev"));
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
@@ -44,17 +38,10 @@ app.use("/api/payment", paymentRoute);
 app.use("/api/sdk", sdkRoutes);
 app.use("/api/notification", notificationRoutes);
 
-// app.listen(PORT, async () => {
-//   await connectToDB();
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-app.use((err, req, res, next) => {
-  console.error("❌ Error:", err.message);
-  res.status(500).json({
-    success: false,
-    message: "Internal Server Error",
-  });
+app.listen(PORT, async () => {
+  await connectToDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
+
